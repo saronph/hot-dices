0f6116aa9b2b940cccd67aff17e6743da0a92e34
Saron Philippi <saron.ph@gmail.com>
2025-05-03 16:14:25 -0300

    start

---
+++ src/constants/Phrases.ts
@@ -0,0 +1,18 @@
+export const Task = [
+  "Beijar ou beliscar",
+  "Abraçar ou se esfregar",
+  "Acariciar ou fazer cócegas",
+  "Tocar ou apertar",
+  "Lamber ou lambuzar",
+  "Chupar ou morder",
+];
+
+export const Local = [
+  "Pescoço",
+  "Peito",
+  "Pernas",
+  "Sexo",
+  "Bunda",
+  "Boca",
+];
+
